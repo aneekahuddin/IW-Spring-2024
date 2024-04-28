8e0f1cb251fc9b5ccaa2112bcad832bb908c5cb9
aneekahuddin <auddin101@gmail.com>
2024-04-27 21:14:35 -0400

    Changes to audiobook

--- AudioInstructions/audioinstructionsscript.py
+++ AudioInstructions/audioinstructionsscript.py
@@ -7,10 +7,10 @@ def createAudioInstructions(phrase, filename):
 
 createAudioInstructions("practice making something", "test")
 
-sBotIntro = "Hi! I'm Storyteller Bot! I can't wait to read with you. If you'd like to listen to a book, press one. If you'd like to read a book, press two. \nYour Option (Press Enter to Continue): "
-
-audiobookScanInstruction = "Storyteller Bot is ready to read with you! Pick a book and place it in front of my eyes. When you're ready, press 1 and enter to continue!"
-
-firstScanInstruction = "Storyteller Bot is ready to read with you! Pick a book and place it in front of my eyes. When you're ready, press 1 and enter to continue!"
+sBotIntro = "Hi! I'm Rhino Reader! I can't wait to read with you. If you'd like to listen to a book, press one. If you'd like to read a book, press two. \nYour Option (Press Enter to Continue): "
+audiobookScanInstruction = "Rhino Reader is ready to read! You have Goldilocks, Little Red Riding Hood, and the Boy Who Cried Wolf in your library. Press 1 to listen to Goldilocks, 2 for Little Red Riding Hood, and 3 for Boy Who Cried Wolf."
+firstScanInstruction = "Rhino Reader is ready to read with you! Pick a book and place it in front of my eyes. When you're ready, press 1 and enter to continue!"
 additionalScanInstruction = "To read another page, press 1 and enter to continue."
 endSessionInstruction = "I loved reading with you! Let's read again next time."
+
+createAudioInstructions(audiobookScanInstruction, "audiobookScanInstruction")
--- audiobook.py
+++ audiobook.py
@@ -20,39 +20,37 @@ Bot logs this book (To send out to parents, To create a post reading list, to al
 
 
 audiobook_files = dict()
-audiobook_files["test book one"] = "AudioBooks/goldilocks-shorter.mp3"
+# Code Cleanup - Find a way to generate audiobook library in a better way
+audiobook_files["Goldilocks"] = "AudioBooks/goldilocks-shorter.mp3"
+audiobook_files["Little Red Riding Hood"] = "AudioBooks/Little-red-riding-hood-even-shorter.mp3"
+audiobook_files["Boy Who Cried Wolf"] = "AudioBooks/storynory-boy-who-cried-wolf.mp3"
 
 from gtts import gTTS 
 import os
-import cv2 
 import random
 
 def audiobook(): 
-    cam_port = 0
-    cap = cv2.VideoCapture(2) # connects to my iPhone camera currently
     os.system("afplay AudioInstructions/audiobookScanInstruction.mp3")
     
     audiobook_filename = ""
+
     # have them press a button when they are ready to read aloud
-    option = input("scan title")
-    if option == "1":
-        ret, img = cap.read() 
-
-        # saving the image 
-        cv2.imwrite("img1.png", img) 
-        cap.release() 
-
-        #audiobook_title = detect_text("img1.png")
-        audiobook_title = "test book one"
-        audiobook_filename = audiobook_files[audiobook_title]
-    if option == "2":
+    option = input("Press 1 to listen to Goldilocks, 2 for Little Red Riding Hood, and 3 for Boy Who Cried Wolf")
+    if option == "0":
         audiobook_filename = random.choice(list(audiobook_files.values()))
-        
+    elif option == "1":
+        audiobook_title = "Goldilocks"
+        audiobook_filename = audiobook_files[audiobook_title]
+    elif option == "2":
+        audiobook_title = "Little Red Riding Hood"
+        audiobook_filename = audiobook_files[audiobook_title]
+    elif option == "2":
+        audiobook_title = "Boy Who Cried Wolf"
+        audiobook_filename = audiobook_files[audiobook_title]
+  
     os_cmd = "afplay " + audiobook_filename
     os.system(os_cmd)
     print("here")
-
-
-    cv2.destroyAllWindows() 
+    option = input("scan") # build in a way to "wait" better. (maybe another loop that pauses? )
 
 '''EXT - Have the book start at where it might have left off in previous cases, have the ability to skip forward in the story, etc.'''
